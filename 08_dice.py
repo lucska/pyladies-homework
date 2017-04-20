a4fa4ff2b0ec186b5202867db2e20c2eb353e7c4
Lucie Zemanova <lucska@gmail.com>
2017-04-20 20:39:38 +0200

    doplnena, funkcni verze

--- 08_dice.py
+++ 08_dice.py
@@ -4,3 +4,11 @@
 # dokud nepadne šestka i jemu. Potom hází hráč třetí, a nakonec čtvrtý. Vyhrává ten, kdo na hození šestky
 # potřeboval nejvíc hodů. (V případě shody vyhraje ten, kdo házel dřív.)
 # Program by měl vypisovat všechny hody, a nakonec napsat, kdo vyhrál.
+
+#nastin...
+while hod != 6:
+    hod = hod_kostkou()
+    # do whilu dat dalsí přiklad, pocitadlo
+
+def hod_kostkou():
+    return random (6) ##dopracovat
